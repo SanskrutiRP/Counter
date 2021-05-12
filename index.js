5b14c53d8df386ec5102b154384b7ea0f66bb9d2
Sanskruti Punde <67673060+SanskrutiRP@users.noreply.github.com>
2021-05-12 23:46:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let zero = 0;
+let rotateY = 180;
+let count1 = 0;
+let container = document.getElementById('container');
+let dec = document.getElementById('dec');
+let inc = document.getElementById('inc');
+let counter = document.getElementById('count');
+let curr_count = count.innerText; 
+dec.addEventListener('click', () =>  {
+    if(count1 % 2 == 0)
+        curr_count--;
+    else
+        curr_count++;
+    container.style.transitionDuration = '.5s';
+    zero-=rotateY;
+    container.style.transform = 'rotateY('+zero+'deg)';
+    updateCounter(curr_count);
+    counter.style.transform = 'rotateY('+zero+'deg)';
+    count1++;
+});
+
+inc.addEventListener('click', () => {
+    if(count1 % 2 == 0)
+        curr_count++;
+    else
+        curr_count--;
+    zero += rotateY;
+    container.style.transitionDuration = '.5s';
+    container.style.transform = 'rotateY(' + zero + 'deg)';
+    updateCounter(curr_count);
+    counter.style.transform = 'rotateY('+zero+'deg)';
+    count1++;
+});
+
+function updateCounter(c){
+    counter.innerText = c;
+}
